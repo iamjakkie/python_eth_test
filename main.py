c348b0ee715f828a9ea0ed380b28eaf7a7193455
jakkie <jakubkielbasiewicz@gmail.com>
2023-02-23 17:58:51 +0100

    getBlockByNumber init

--- main.py
+++ main.py
@@ -8,10 +8,26 @@ ALCHEMY_BASE_URL = "https://eth-mainnet.g.alchemy.com/v2/"
 async def get_block_data(block_no:str = "latest"):
     api_key = os.environ["ALCHEMY_API_KEY"]
     url = ALCHEMY_BASE_URL + api_key
+    payload = {
+        "id": 1,
+        "jsonrpc": "2.0",
+        "method": "eth_getBlockByNumber",
+        "params": ['0x1b4', True]
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json"
+    }
 
-    w3 = Web3(Web3.HTTPProvider(url))
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=payload, headers=headers) as resp:
+            print(await resp.text())
 
-    print(w3.eth.get_block(block_no))
+    # w3 = Web3(Web3.HTTPProvider(url))
+
+    # print(w3.eth.get_block(block_no))
+
+
 
 
 async def main():
